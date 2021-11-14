715c76b847321e8b09352eec5a0bf8a811d30883
Yun Ho Jung <YunHo.Jung@ibm.com>
2021-11-14 10:05:18 -0600

    add ml

--- backend.py
+++ backend.py
@@ -3,26 +3,24 @@ import json
 import re
 
 from flask_cors import CORS
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_restful import Resource, Api
 from json import dumps
+import couchdb
 
+from nltk.corpus import wordnet
 
 bad_words_txt = open('badwords.txt', 'r').read()
-bad_words_arr = bad_words_txt.splitlines()
-print(bad_words_arr)
+bad_words_arr = bad_words_txt.splitlines() 
 
+common_words_text = open('google-10000-english.txt', 'r').read()
+common_words_arr = common_words_text.splitlines()
 
-
-class REMOVE_BAD_WORDS(Resource):
-    def get(self, target_string):
-        for bad_word in bad_words_arr:
-           target_string = re.sub(bad_word, '****', target_string)
-        return {
-            "data" : target_string
-        }
-
-
+couch = couchdb.Server('http://admin:password@127.0.0.1:5984/')
+print('reach')
+for dbname in couch:
+    print(dbname)
+db = couch['simpleready']
 
 app = Flask(__name__)
 api = Api(app)
@@ -34,6 +32,17 @@ CORS(app)
 def success(name):
    return 'welcome %s' % name
 
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    docs = list(db.find({'selector': {'username': username, 'password': password}}))
+    print(docs)
+    if len(docs) == 0:
+        abort(404)
+    else:
+        return docs[0]
+
 @app.route('/badwords',methods = ['GET'])
 def badwords():
     target_string = request.args.get('target_string')
@@ -43,6 +52,39 @@ def badwords():
     response = jsonify({"data" : target_string})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+@app.route('/simplify', methods = ['GET'])
+def simplify():
+    rVal = ""
+    target_string = request.args.get('target_string')
+    replacement_dictionary={}
+    target_string_lines = target_string.splitlines()
+    for target_line in target_string_lines:
+        target_string_words = target_line.split()
+        for target_word in target_string_words:
+            if target_word.lower() not in common_words_arr:
+                if target_word.lower() not in replacement_dictionary:
+                    syn = synonym(target_word.lower())
+                    if not syn == target_word.lower():
+                        replacement_dictionary[target_word.lower()] = syn
+                        rVal += replacement_dictionary[target_word.lower()] + " "
+                    else:
+                        rVal += target_word.lower() + " "
+                else:
+                    rVal += replacement_dictionary[target_word.lower()] + " "
+
+            else:
+                rVal += target_word + " "
+        rVal += "\n"
+    response = jsonify({"data" : rVal, "dict" : replacement_dictionary})
+    return response
+
+def synonym(target):
+    synList = wordnet.synsets(target)
+    if (len(synList) > 1):
+        return synList[0].name()[0:synList[0].name().find('.')]
+    else:
+        return target
 
 MYPORT = sys.argv[1]
 
